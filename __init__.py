1bdeb81fecf69a213ccbaaaae21c6fa5f0db1b65
Sascha Vis <saschavis@gmail.com>
2020-11-02 14:30:54 +0100

    let the client reconnect when the connection is interrupted

--- __init__.py
+++ __init__.py
@@ -10,11 +10,14 @@ from mycroft.util import time as m_time
 
 
 import websocket
-import _thread
-import time
+import _thread, time
 import json
 
+import gc
+
 from datetime import datetime, timedelta
+
+ws_connected = False
 
 print('Setting up client to connect to a local mycroft instance')
 client = MessageBusClient()
@@ -23,6 +26,7 @@ client.run_in_thread()
 stopSig = False
 
 device_id = "ZOD_69XXX420"
+ws_url = "ws://yeplab.com:6456/"
 
 # Define WebSocket callback functions
 def ws_message(ws, message):
@@ -60,7 +64,9 @@ def ws_open(ws):
 
 def on_close(ws):
     LOG.info("### socket closed ###")
-    
+
+def on_error(ws):
+    pass
 
 class Zod(MycroftSkill):
     def __init__(self):
@@ -76,16 +82,33 @@ class Zod(MycroftSkill):
     
     def initialize_websocket(self):
         # Start a new thread for the WebSocket interface
-        _thread.start_new_thread(self.ws_thread, ())
+        _thread.start_new_thread(self.__create_ws, ())
     
     # def _speak_dialog(self, dialog, data=None, response =False):
     #     self.speak_dialog(dialog, data, response)
 
-    def ws_thread(self, *args):
-        ws = websocket.WebSocketApp("ws://192.168.86.48:8123/", on_open = ws_open, on_message = ws_message, on_close= on_close)
-        ws.run_forever()
-
-
+    # def ws_thread(self, *args):
+    #     ws = websocket.WebSocketApp("ws://yeplab.com:6456/", on_open = ws_open, on_message = ws_message, on_close= on_close)
+    #     ws.run_forever()
+    
+    def __create_ws(self):
+        while True:
+            try:
+                websocket.enableTrace(False)
+                self.__WSCONNECTION = websocket.WebSocketApp(ws_url,
+                                          on_open=ws_open,
+                                          on_message = ws_message,
+                                          on_error = on_error,
+                                          on_close = on_close
+                                          )
+                self.__WSCONNECTION.on_open = self.__on_open
+                self.__WSCONNECTION.run_forever(skip_utf8_validation=True,ping_interval=10,ping_timeout=8)
+            except Exception as e:
+                gc.collect()
+                LOG.debug("Websocket connection Error  : {0}".format(e))                    
+            LOG.debug("Reconnecting websocket  after 5 sec")
+            time.sleep(5)
+    
 def create_skill():
     return Zod()
 
